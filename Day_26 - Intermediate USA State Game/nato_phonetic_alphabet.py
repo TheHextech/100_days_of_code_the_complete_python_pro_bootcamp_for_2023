f173f05fe967eb33d122a517d3d278ec40762d32
Emanuele Immesi <emanueleimmesi@gmail.com>
2023-02-11 12:02:58 +0100

    updated nato phonetic alphabet with exceptions of lesson 30

--- Day_26 - Intermediate USA State Game/nato_phonetic_alphabet.py
+++ Day_26 - Intermediate USA State Game/nato_phonetic_alphabet.py
@@ -1,12 +1,29 @@
-# TODO 1. Create a dictionary in this format {"A": "Alfa", "B": "Bravo"}
-
 import pandas
 
 alphabet_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dictionary = {row.letter: row.code for (index, row) in alphabet_data_frame.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ").upper()
-output = [alphabet_dictionary[letter] for letter in user_input]
+def generate_phonetics():
 
-print(output)
+    """This function was generated in lesson 30, updating the code developed in lesson 26. This version
+    also provide a repeat in case the user entered a null value."""
+
+    user_input = input("\nEnter a word: ").upper()
+
+    if user_input == '':
+        print("Insert at least one letter please.\n")
+        return generate_phonetics()
+
+    else:
+
+        try:
+            output = [alphabet_dictionary[letter] for letter in user_input]
+
+        except KeyError:
+            print("Sorry, only letters in the alphabet please.\n")
+            generate_phonetics()
+
+        else:
+            print(output)
+
+generate_phonetics()
